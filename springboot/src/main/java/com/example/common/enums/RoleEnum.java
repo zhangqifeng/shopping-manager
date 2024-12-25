06ff19d24c1ba86a82b7cd082bec6ca9e89af8c4
kihi001 <1528832697@qq.com>
2024-12-25 16:34:15 +0900

    1

--- springboot/src/main/java/com/example/common/enums/RoleEnum.java
+++ springboot/src/main/java/com/example/common/enums/RoleEnum.java
@@ -3,4 +3,8 @@ package com.example.common.enums;
 public enum RoleEnum {
     // 管理员
     ADMIN,
+    // 商家
+    BUSINESS,
+    // 用户
+    USER,
 }
--- vue/src/views/Manager.vue
+++ vue/src/views/Manager.vue
@@ -42,15 +42,16 @@
             <template slot="title">
               <i class="el-icon-menu"></i><span>信息管理</span>
             </template>
-            <el-menu-item index="/notice">公告信息</el-menu-item>
-            <el-menu-item index="/type">分类信息</el-menu-item>
+            <el-menu-item v-if="user.role === 'ADMIN'" index="/notice">公告信息</el-menu-item>
+            <el-menu-item v-if="user.role === 'ADMIN'" index="/type">分类信息</el-menu-item>
           </el-submenu>
 
-          <el-submenu index="user">
+          <el-submenu index="user" v-if="user.role === 'ADMIN'">
             <template slot="title">
               <i class="el-icon-menu"></i><span>用户管理</span>
             </template>
             <el-menu-item index="/admin">管理员信息</el-menu-item>
+            <el-menu-item index="/business">商家信息</el-menu-item>
           </el-submenu>
         </el-menu>
       </div>
@@ -84,6 +85,9 @@ export default {
     goToPerson() {
       if (this.user.role === 'ADMIN') {
         this.$router.push('/adminPerson')
+      }
+      if (this.user.role === 'BUSINESS') {
+        this.$router.push('/businessPerson')
       }
     },
     logout() {
