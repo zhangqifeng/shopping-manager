133a4fefde1d2ce58f216028eb4e0f53ea3cc6f2
Your Name <you@example.com>
2024-12-26 21:12:47 +0900

    二次完善

--- manager/springboot/src/main/java/com/example/controller/WebController.java
+++ manager/springboot/src/main/java/com/example/controller/WebController.java
@@ -7,6 +7,7 @@ import com.example.common.enums.ResultCodeEnum;
 import com.example.common.enums.RoleEnum;
 import com.example.entity.Account;
 import com.example.service.AdminService;
+import com.example.service.BusinessService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -19,6 +20,8 @@ public class WebController {
 
     @Resource
     private AdminService adminService;
+    @Resource
+    private BusinessService businessService;
 
     @GetMapping("/")
     public Result hello() {
@@ -37,6 +40,9 @@ public class WebController {
         if (RoleEnum.ADMIN.name().equals(account.getRole())) {
             account = adminService.login(account);
         }
+        if (RoleEnum.BUSINESS.name().equals(account.getRole())) {
+            account = businessService.login(account);
+        }
         return Result.success(account);
     }
 
@@ -52,6 +58,9 @@ public class WebController {
         if (RoleEnum.ADMIN.name().equals(account.getRole())) {
             adminService.register(account);
         }
+        if (RoleEnum.BUSINESS.name().equals(account.getRole())) {
+            businessService.register(account);
+        }
         return Result.success();
     }
 
@@ -66,6 +75,9 @@ public class WebController {
         }
         if (RoleEnum.ADMIN.name().equals(account.getRole())) {
             adminService.updatePassword(account);
+        }
+        if (RoleEnum.BUSINESS.name().equals(account.getRole())) {
+            businessService.updatePassword(account);
         }
         return Result.success();
     }
---
+++ manager/springboot/src/main/java/com/example/service/BusinessService.java
@@ -0,0 +1,30 @@
+package com.example.service;
+
+import com.example.common.enums.RoleEnum;
+import com.example.entity.Account;
+import com.example.entity.Address;
+import com.example.mapper.AddressMapper;
+import com.example.utils.TokenUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class BusinessService {
+
+
+    public Account login(Account account) {
+        return null;
+    }
+
+    public void register(Account account) {
+
+    }
+
+    public void updatePassword(Account account) {
+
+    }
+}
